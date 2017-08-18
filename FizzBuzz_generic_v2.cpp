d7edfa07fe4a3795b8fb33468eae15c11faf0533
dgquvn <dgquvn@gmail.com>
2017-08-18 17:15:18 -0400

    Add files via upload

---
+++ FizzBuzz_generic_v2.cpp
@@ -0,0 +1,71 @@
+//
+//  main.cpp
+//  FizzBuzz_generic_v2
+//
+//  Created by Guoqiang Deng on 8/18/17.
+//  Copyright © 2017 Guoqiang Deng. All rights reserved.
+//
+
+// This is the generic program for print FizzBuzz
+// More details can be found in README.md 3nd problem definition
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctype.h>
+#include <vector>
+using namespace std;
+
+string strRead(const string& s);
+
+int main(){
+    ifstream inputFile("input.txt");
+    vector<string> inputStr(5);
+    if (inputFile.is_open()){
+        for (int i = 0; i < 5; i++){
+            string line;
+            getline(inputFile, line);
+            inputStr[i] = strRead(line);
+        }
+        
+        inputFile.close();
+    }
+    
+    int max_Int = stoi(inputStr[0]);
+    int lower_divisor = stoi(inputStr[1]);
+    int upper_divisor = stoi(inputStr[2]);
+    string lower_div_lab = inputStr[3];
+    string upper_div_lab = inputStr[4];
+    if (lower_divisor > upper_divisor){
+        swap(lower_divisor, upper_divisor);
+        swap(lower_div_lab, upper_div_lab);
+    }
+    
+    ofstream outputFile("output.txt", ios::out);
+    if (outputFile.is_open()){
+        for (int i = 1; i <= max_Int; i++){
+            string output_str;
+            if (i % lower_divisor == 0)
+                output_str = lower_div_lab;
+            if (i % upper_divisor == 0)
+                output_str += upper_div_lab;
+            if (output_str.size() == 0)
+                output_str = to_string(i);
+            outputFile << output_str << "\n";
+        }
+        outputFile.close();
+    }
+    return 0;
+}
+
+string strRead(const string& s){
+    auto pos = s.find('=');
+    string res = s.substr(pos+1, s.size()-pos-1);
+    while(res[0] == ' '){
+        res.erase(res.begin());
+    }
+    while(res.back() == ' '){
+        res.pop_back();
+    }
+    return res;
+}
